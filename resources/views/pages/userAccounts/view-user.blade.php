3a3428352ab019e9b1af5518367e1c1b9e3c2668
Mary Angel <201910994@fit.edu.ph>
2023-06-08 10:55:52 +0800

    asd

--- resources/views/pages/userAccounts/view-user.blade.php
+++ resources/views/pages/userAccounts/view-user.blade.php
@@ -22,13 +22,13 @@
 
                     <div class="flex-wrap items-center md:grid md:grid-cols-12 w-full">
                         <label for="middle_name" class="md:col-span-2 whitespace-nowrap">Middle Name:</label>
-                        <input type="text" name="middle_name" value="{{$viewUser->middle_name}}" disabled {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
+                        <input type="text" name="middle_name" value="{{$viewUser->middle_name}}" {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
                         @error ('middle_name') <p class="md:col-start-4 md:col-span-10 text-sm text-red -mb-3"> {{$message}} </p> @enderror
                     </div>
 
                     <div class="flex-wrap items-center md:grid md:grid-cols-12 w-full">
                         <label for="last_name" class="md:col-span-2 whitespace-nowrap">Last Name:</label>
-                        <input type="text" name="last_name" value="{{$viewUser->last_name}}" disabled {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
+                        <input type="text" name="last_name" value="{{$viewUser->last_name}}" {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
                         @error ('last_name') <p class="md:col-start-4 md:col-span-10 text-sm text-red -mb-3"> {{$message}} </p> @enderror
                     </div>
 
@@ -52,26 +52,24 @@
 
                     <div class="flex-wrap items-center md:grid md:grid-cols-12 w-full">
                         <label for="birthday" class="md:col-span-2 whitespace-nowrap">Birthday:</label>
-                        <input type="date" name="birthday" value="{{$viewUser->birthday}}" disabled {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
+                        <input type="date" name="birthday" value="{{$viewUser->birthday}}" {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
                         @error ('birthday') <p class="md:col-start-4 md:col-span-10 text-sm text-red -mb-3"> {{$message}} </p> @enderror
                     </div>
 
                     <div class="flex-wrap items-center md:grid md:grid-cols-12 w-full">
                         <label for="contact" class="md:col-span-2 whitespace-nowrap">Mobile No:</label>
-                        <input type="text" name="contact" value="{{$viewUser->contact}}" disabled {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
+                        <input type="text" name="contact" value="{{$viewUser->contact}}" {{$errors->any() ? '' : 'disabled'}} class="form-input md:col-start-4 md:col-span-10 focus:ring-0:mb-0">
                         @error ('contact') <p class="md:col-start-4 md:col-span-10 text-sm text-red -mb-3"> {{$message}} </p> @enderror
                     </div>
 
                     <div class="flex-wrap items-center md:grid md:grid-cols-12 w-full">
                         <label for="email" class="md:col-span-2 whitespace-nowrap">Email Address:</label>
-                        <input type="text" name="email" value="{{$viewUser->email}}" disabled class="form-input md:col-start-4 md:col-span-10 focus:ring-0 text-disabled-text bg-disabled-bg disabled:mb-0">
+                        <input type="text" name="email" value="{{$viewUser->email}}" class="form-input md:col-start-4 md:col-span-10 focus:ring-0 text-disabled-text bg-disabled-bg disabled:mb-0">
                         @error ('email') <p class="md:col-start-4 md:col-span-10 text-sm text-red -mb-3"> {{$message}} </p> @enderror
                     </div>
                 </div>
                 
                 <div class="flex gap-3 justify-end p-3">
-                    
-                    {{-- <a href="{{url('/users')}}" id="closeButton" class="close-btn bg-gray-200 text-black text-sm rounded-full px-4 py-2 hover:bg-black hover:text-white">Close</a> --}}
                     <button id="closeButton" type="button" class="close-btn bg-gray-200 text-black text-sm rounded-full px-4 py-2 hover:bg-black hover:text-white">{{$errors->any() ? 'Cancel' : 'Close'}}</button>
                     <button id="editButton" class="text-white bg-primary px-4 py-2 rounded-full hover:bg-white hover:text-primary hover:ring-primary hover:ring-1 duration-100">{{$errors->any() ? 'Save' : 'Edit'}}</button>
                 </div>
