2281d26341cee42090287e0bd69e83e4c0a661cd
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2023-06-22 13:23:18 +0800

    user export header added

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,14 +4,57 @@ namespace App\Exports;
 
 use App\Models\User;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder;
+use Maatwebsite\Excel\Concerns\Exportable;
 
-class UsersExport implements FromCollection
+class UsersExport extends DefaultValueBinder implements FromCollection, WithHeadings, ShouldAutoSize
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    use Exportable;
+
+    public function headings() :array 
+    {
+        return 
+        [
+            [
+                'username',
+                'first_name',
+                'middle_name',
+                'last_name',
+                'role',
+                'specialization',
+                'barangay',
+                'birthday',
+                'contact',
+                'email', 
+                'security_question',
+                'security_answer',
+                'status',
+                'created_at',
+                'updated_at'
+            ]
+        ];
+    }  
+
     public function collection()
     {
-        return User::all();
+        return User::select(
+            'username',
+            'first_name',
+            'middle_name',
+            'last_name',
+            'role',
+            'specialization',
+            'barangay',
+            'birthday',
+            'contact',
+            'email', 
+            'security_question',
+            'security_answer',
+            'status',
+            'created_at',
+            'updated_at'
+        )->get();
     }
 }
