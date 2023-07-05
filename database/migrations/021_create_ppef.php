191c2368e6f5ca8d52730bfff1fc67124c128f2f
allenJcodes <110220645+allenJcodes@users.noreply.github.com>
2023-07-05 15:35:13 +0800

    migration file name change

--- database/migrations/021_create_ppef.php
+++ database/migrations/021_create_ppef.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('ppef', function (Blueprint $table){
             $table->id();
             $table->integer('one_ef_client_id')->unsigned();
-            // $table->unsignedBigInteger('one_ef_client_id');   
             $table->foreign('one_ef_client_id')->references('id')->on('one_ef_client')->onDelete('cascade');
             $table->integer('ONE_EF_BPSYSTOLIC')->nullable();
             $table->integer('ONE_EF_BPDIASTOLIC')->nullable();
